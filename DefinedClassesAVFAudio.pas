6cc6f4bd9ad8d2901c9435ed2ed78dbac1910581
Ryan Joseph <genericptr@gmail.com>
2021-09-03 11:30:56 -0600

    AVAudio (missing files)

---
+++ DefinedClassesAVFAudio.pas
@@ -0,0 +1,79 @@
+{$mode delphi}
+{$modeswitch objectivec1}
+{$modeswitch cvar}
+{$packrecords c}
+
+unit DefinedClassesAVFAudio;
+interface
+
+type
+  AVAudioBuffer = objcclass external;
+  AVAudioChannelLayout = objcclass external;
+  AVAudioCompressedBuffer = objcclass external;
+  AVAudioConnectionPoint = objcclass external;
+  AVAudioConverter = objcclass external;
+  AVAudioEngine = objcclass external;
+  AVAudioEnvironmentDistanceAttenuationParameters = objcclass external;
+  AVAudioEnvironmentNode = objcclass external;
+  AVAudioEnvironmentReverbParameters = objcclass external;
+  AVAudioFile = objcclass external;
+  AVAudioFormat = objcclass external;
+  AVAudioIONode = objcclass external;
+  AVAudioInputNode = objcclass external;
+  AVAudioMixerNode = objcclass external;
+  AVAudioMixingDestination = objcclass external;
+  AVAudioNode = objcclass external;
+  AVAudioOutputNode = objcclass external;
+  AVAudioPCMBuffer = objcclass external;
+  AVAudioPlayer = objcclass external;
+  AVAudioPlayerNode = objcclass external;
+  AVAudioRecorder = objcclass external;
+  AVAudioRoutingArbiter = objcclass external;
+  AVAudioSequencer = objcclass external;
+  AVAudioSession = objcclass external;
+  AVAudioSessionChannelDescription = objcclass external;
+  AVAudioSessionDataSourceDescription = objcclass external;
+  AVAudioSessionPortDescription = objcclass external;
+  AVAudioSessionRouteDescription = objcclass external;
+  AVAudioSinkNode = objcclass external;
+  AVAudioSourceNode = objcclass external;
+  AVAudioTime = objcclass external;
+  AVAudioUnit = objcclass external;
+  AVAudioUnitComponent = objcclass external;
+  AVAudioUnitComponentManager = objcclass external;
+  AVAudioUnitDelay = objcclass external;
+  AVAudioUnitDistortion = objcclass external;
+  AVAudioUnitEQ = objcclass external;
+  AVAudioUnitEQFilterParameters = objcclass external;
+  AVAudioUnitEffect = objcclass external;
+  AVAudioUnitGenerator = objcclass external;
+  AVAudioUnitMIDIInstrument = objcclass external;
+  AVAudioUnitReverb = objcclass external;
+  AVAudioUnitSampler = objcclass external;
+  AVAudioUnitTimeEffect = objcclass external;
+  AVAudioUnitTimePitch = objcclass external;
+  AVAudioUnitVarispeed = objcclass external;
+  AVMIDIPlayer = objcclass external;
+  AVMusicTrack = objcclass external;
+  AVSpeechSynthesisVoice = objcclass external;
+  AVSpeechSynthesizer = objcclass external;
+  AVSpeechUtterance = objcclass external;
+  AVAudio3DMixingProtocol = objcprotocol external name 'AVAudio3DMixing';
+  AVAudioMixingProtocol = objcprotocol external name 'AVAudioMixing';
+  AVAudioPlayerDelegateProtocol = objcprotocol external name 'AVAudioPlayerDelegate';
+  AVAudioRecorderDelegateProtocol = objcprotocol external name 'AVAudioRecorderDelegate';
+  AVAudioSessionDelegateProtocol = objcprotocol external name 'AVAudioSessionDelegate';
+  AVAudioStereoMixingProtocol = objcprotocol external name 'AVAudioStereoMixing';
+  AVSpeechSynthesizerDelegateProtocol = objcprotocol external name 'AVSpeechSynthesizerDelegate';
+
+type
+  AUAudioUnit = objcclass external;
+  AVMusicTrackEventIterator = objcclass external;
+  NSData = objcclass external;
+  NSError = objcclass external;
+  NSNumber = objcclass external;
+  NSString = objcclass external;
+  NSURL = objcclass external;
+
+implementation
+end.
